2838c7878ca3b497752d09a0ca1e08214575d3d9
Saravana Kumar Anbarasu <saravana.anbarasu@gmail.com>
2023-02-24 11:54:01 +0530

    Update pyspark-rdd.py

--- labs/pyspark/pyspark-examples/pyspark-rdd.py
+++ labs/pyspark/pyspark-examples/pyspark-rdd.py
@@ -4,7 +4,7 @@ from pyspark.sql import SparkSession
 spark = SparkSession.builder.appName('pyspark-examples').getOrCreate()
 
 print(spark)
-rdd=spark.sparkContext.parallelize([1,2,3,4,56])
+rdd=spark.sparkContext.parallelize([1,2,3,4,5,6])
 print("RDD count :"+str(rdd.count()))
 
 rdd = spark.sparkContext.emptyRDD
